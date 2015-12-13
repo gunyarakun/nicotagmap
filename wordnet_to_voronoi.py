c56dd6af3106a85c11c2d5f4637f749a11c0d3f6
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2015-12-13 13:25:49 +0900

    Follow up latest igraph

--- wordnet_to_voronoi.py
+++ wordnet_to_voronoi.py
@@ -8,31 +8,26 @@ import math
 import string
 
 MERGE_ADJACENT_LEAF = True
-LOG_AREA = True
-CONST_AREA = False
 
 def main(infile):
-  g = Graph.Read_Lgl(infile)
+  # 無向グラフとしてLGLを読み込む
+  g = Graph.Read_Lgl(infile, directed=False)
+  # コミュニティのデンドログラムを作る
   d = g.community_fastgreedy()
+  # タグ名(動画数)となっているので、それを取り出す
   r = re.compile(r'(?P<name>.+)\((?P<count>\d+)\)')
 
-  results = [None] * d._n
-  inorder = d._traverse_inorder()
+  results = [None] * g.vcount()
   cstrs = []
-  for elem in range(d._n):
+  for elem in range(g.vcount()):
     matched = r.match(g.vs[elem]['name'])
     sc = {
       'area': int(matched.group('count')),
       'leaf': True,
       'children': [elem],
     }
-    if LOG_AREA:
-      sc['area'] = math.log(sc['area'], 1.1)
-    if CONST_AREA:
-      sc['area'] = CONST_AREA
 
-    escaped_string = string.replace(matched.group('name'), '\\', '\\\\')
-    # FIXME: escape name string!!!
+    escaped_string = matched.group('name').replace('\\', '\\\\')
     cstrs.append('TreeNode tn%d("%s", %f);' % (elem,
                                                escaped_string,
                                                sc['area']))
@@ -43,35 +38,38 @@ def main(infile):
     nn = len(results)
     n1 = results[v1]
     n2 = results[v2]
+
     # if n1 is None or n2 is None: continue
-    sc = {
+    merge_node = {
       'area': n1['area'] + n2['area'],
       'leaf': False,
-      'children': [],
+      'children': [v1, v2],
     }
 
     if MERGE_ADJACENT_LEAF:
       if n1['leaf'] and n2['leaf']:
-        sc['children'].append(v1)
-        sc['children'].append(v2)
+        merge_node['children'].append(v1)
+        merge_node['children'].append(v2)
       elif n1['leaf'] and not n2['leaf']:
-        sc['children'].append(v1)
-        sc['children'].extend(n2['children'])
+        merge_node['children'].append(v1)
+        merge_node['children'].extend(n2['children'])
       elif not n1['leaf'] and n2['leaf']:
-        sc['children'].extend(n1['children'])
-        sc['children'].append(v2)
+        merge_node['children'].extend(n1['children'])
+        merge_node['children'].append(v2)
       else:
-        sc['children'].append(nn)
-        cstrs.append('TreeNode tn%d(NULL, %f);' % (nn, sc['area']))
+        print("kitaktak")
+        merge_node['children'].append(nn)
+        cstrs.append('TreeNode tn%d(NULL, %f);' % (nn, merge_node['area']))
         for idx in n1['children']:
           cstrs.append('tn%d.add_child(tn%d);' % (nn, idx))
         for idx in n2['children']:
           cstrs.append('tn%d.add_child(tn%d);' % (nn, idx))
     else:
-      cstrs.append('TreeNode tn%d(NULL, %f);' % (nn, sc['area']))
+      cstrs.append('TreeNode tn%d(NULL, %f);' % (nn, merge_node['area']))
       cstrs.append('tn%d.add_child(tn%d);' % (nn, v1))
       cstrs.append('tn%d.add_child(tn%d);' % (nn, v2))
-    results.append(sc)
+
+    results.append(merge_node)
 
   cstrs.append('#define ROOT_TREE_NODE tn%d' % nn)
   print('\n'.join(cstrs))
